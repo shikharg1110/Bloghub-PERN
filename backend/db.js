34dcc48e3544480704bd8bb40ac46ec2fe486855
Shikhar <shikharg1110@gmail.com>
2024-09-30 12:02:57 +0530

    deploy

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,24 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool( {
-    user: "postgres",
-    password: "12345",
-    host: "localhost",
+    user: "postgress",
+    password: "xW0hlU1Wf9gBBCs9KNO5VPMTpWUHRLRA",
+    host: "postgresql://postgress:xW0hlU1Wf9gBBCs9KNO5VPMTpWUHRLRA@dpg-crt48be8ii6s73ehujc0-a/bloghub_zfdi",
     port: 5432,
-    database: "bloghub"
+    database: "bloghub_zfdi"
 })
 
 module.exports = pool;
+
+
+// const Pool = require('pg').Pool;
+
+// const pool = new Pool( {
+//     user: "postgres",
+//     password: "12345",
+//     host: "localhost",
+//     port: 5432,
+//     database: "bloghub"
+// })
+
+// module.exports = pool;
