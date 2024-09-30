2bce19c2eafeae262e9f66109ccffc12019903c3
Shikhar <shikharg1110@gmail.com>
2024-09-30 12:32:07 +0530

    env change

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,16 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
-const pool = new Pool( {
-    user: "postgress",
-    password: "xW0hlU1Wf9gBBCs9KNO5VPMTpWUHRLRA",
-    host: "postgresql://postgress:xW0hlU1Wf9gBBCs9KNO5VPMTpWUHRLRA@dpg-crt48be8ii6s73ehujc0-a/bloghub_zfdi",
-    port: 5432,
-    database: "bloghub_zfdi"
-})
+// const pool = new Pool( {
+//     user: "postgress",
+//     password: "xW0hlU1Wf9gBBCs9KNO5VPMTpWUHRLRA",
+//     host: "postgresql://postgress:xW0hlU1Wf9gBBCs9KNO5VPMTpWUHRLRA@dpg-crt48be8ii6s73ehujc0-a/bloghub_zfdi",
+//     port: 5432,
+//     database: "bloghub_zfdi"
+// })
 
-module.exports = pool;
+// module.exports = pool;
 
-
-// const Pool = require('pg').Pool;
 
 // const pool = new Pool( {
 //     user: "postgres",
@@ -20,5 +19,15 @@ module.exports = pool;
 //     port: 5432,
 //     database: "bloghub"
 // })
-
 // module.exports = pool;
+
+
+const pool = new Pool( {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+})
+
+module.exports = pool;
