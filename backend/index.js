e6cd422b1b9a1ccc275df44aeb7ab4a554cdf465
Shikhar <shikharg1110@gmail.com>
2024-10-01 13:11:31 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ const { editTag } = require('./routes/editTag');
 const { deleteTagById } = require('./routes/deleteTagById');
 
 const corsOption = {
-    origin: ['https://bloghub-pern.vercel.app', 'http://localhost:5173', 'https://bloghub-pern-11.onrender.com'],
+    origin: ['https://bloghub-pern.vercel.app', 'http://localhost:5173', 'https://bloghub-pern-11.onrender.com', 'https://bloghub-pern.onrender.com/'],
     credentials: true,
     optionSuccessStatus: 200
 }
