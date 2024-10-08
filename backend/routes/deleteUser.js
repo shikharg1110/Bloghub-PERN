ef12a9d156135291dc0828e5b3757dda0f60b852
Shikhar <shikharg1110@gmail.com>
2024-10-08 11:51:47 +0530

    only show active user

--- backend/routes/deleteUser.js
+++ backend/routes/deleteUser.js
@@ -4,15 +4,15 @@ const deleteUser = async(req, res) => {
     try {
         const{ userId } = req.body;
         console.log(userId);
-        const response = await pool.query("DELETE FROM users WHERE user_id = $1", [userId]);
+        const response = await pool.query("UPDATE users SET is_active = FALSE WHERE user_id = $1", [userId]);
         if(response.rowCount > 0)
-            res.status(200).send("user successfully deleted");
+            res.status(200).send("user successfully deactivated");
         else
             res.status(404).send("user not deleted");
     }
     catch(err) {
-        console.log("Error in deleting the user: ", err);
-        return res.status(500).send("Error in deleting the user");
+        console.log("Error in deactivating the user: ", err);
+        return res.status(500).send("Error in deactivating the user");
     }
 }
 
--- backend/routes/getUser.js
+++ backend/routes/getUser.js
@@ -2,7 +2,7 @@ const pool = require('../db');
 
 const getUser = async(req, res) => {
     try {
-        const user = await pool.query("SELECT * FROM users");
+        const user = await pool.query("SELECT * FROM users WHERE is_active = TRUE");
         res.status(200).send(user.rows);
     }
     catch(err) {
